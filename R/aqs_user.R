98d2407c88208e8c58c7dd5adeebbe79a573b62c
Roger D. Peng <rdpeng@jhu.edu>
2019-10-16 16:58:18 -0400

    Allow email/key information to be stored as environment variables

--- R/aqs_user.R
+++ R/aqs_user.R
@@ -3,11 +3,20 @@
 ##' @param email Email address of user
 ##' @param key Key phrase of user
 ##' @details The AQS API requires an email address and key for all queries. The key is not used for authentication (as in a password), but it is used for identification.
+##' @details The email and key can be stored as environment variables as AQS_EMAIL and AQS_KEY, respectively, rather than specified as function arguments.
 ##' @return \code{create_user} returns a list containing the email and key.
 ##' @seealso \code{\link{aqs_get}}
 ##' @export
 create_user <- function(email,
-                     key){
+                        key) {
+    if(missing(email)) {
+        email <- Sys.getenv("AQS_EMAIL")
+        if(!nzchar(email))
+            stop("need AQS email address identifier")
+        key <- Sys.getenv("AQS_KEY")
+        if(!nzchar(key))
+            stop("need AQS key")
+    }
     list(email=email,
          key=key)
 }
